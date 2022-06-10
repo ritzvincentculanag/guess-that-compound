651575b5a55ca973c9ce70662f0e361d647b2e56
ritznotcracker <ritznotcracker@gmail.com>
2022-06-10 19:42:29 +0800

    add: initial take quiz logic

--- app/src/main/java/tech/stargeneration/gtc/screens/ChooseCompounds.java
+++ app/src/main/java/tech/stargeneration/gtc/screens/ChooseCompounds.java
@@ -1,5 +1,7 @@
 package tech.stargeneration.gtc.screens;
 
+import android.content.Intent;
+import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,11 +15,20 @@ public class ChooseCompounds extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_compounds);
 
+        Intent takeQuiz = new Intent(this, TakeQuiz.class);
         RelativeLayout btnIonic = findViewById(R.id.btnIonic);
-        TextView textView = findViewById(R.id.textView);
+        RelativeLayout btnCovalent = findViewById(R.id.btnCovalent);
 
-        btnIonic.setOnClickListener(event -> {
-            textView.setText("Ionic");
+        int takeIonic = 0;
+        int takeCovalent = 1;
+
+        btnIonic.setOnClickListener(ionic -> {
+            takeQuiz.putExtra("QUIZ_TYPE", takeIonic);
+            startActivity(takeQuiz);
+        });
+        btnCovalent.setOnClickListener(covalent -> {
+            takeQuiz.putExtra("QUIZ_TYPE", takeCovalent);
+            startActivity(takeQuiz);
         });
     }
 }
--- app/src/main/java/tech/stargeneration/gtc/screens/TakeQuiz.java
+++ app/src/main/java/tech/stargeneration/gtc/screens/TakeQuiz.java
@@ -1,5 +1,6 @@
 package tech.stargeneration.gtc.screens;
 
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import tech.stargeneration.gtc.R;
@@ -10,5 +11,15 @@ public class TakeQuiz extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_quiz);
+
+        Bundle extras = getIntent().getExtras();
+        TextView test = findViewById(R.id.test);
+
+        int result = extras.getInt("QUIZ_TYPE");
+        if (result == 0) {
+            test.setText("Ionic");
+        } else {
+            test.setText("Covalent");
+        }
     }
 }
