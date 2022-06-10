c1f4fdddbf9126c6286c865450e39ca6ba068628
ritznotcracker <ritznotcracker@gmail.com>
2022-06-10 20:54:58 +0800

    finish: ionic compounds

--- app/src/main/java/tech/stargeneration/gtc/models/CompoundIonic.java
+++ app/src/main/java/tech/stargeneration/gtc/models/CompoundIonic.java
@@ -1,15 +1,22 @@
 package tech.stargeneration.gtc.models;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Random;
 
 public class CompoundIonic {
 
-    Compound ionic1 = new Compound("NaCl", "sodium chloride");
-    Compound ionic2 = new Compound("KBr", "potassium bromide");
-    Compound ionic3 = new Compound("CaI2", "calcium iodide");
-    Compound ionic4 = new Compound("CsF", "cesium fluoride");
-    Compound ionic5 = new Compound("LiCl", "lithium chloride");
+    Compound ionic1 = new Compound("NaCl", "Sodium Chloride");
+    Compound ionic2 = new Compound("KBr", "Potassium Bromide");
+    Compound ionic3 = new Compound("CaI2", "Calcium Iodide");
+    Compound ionic4 = new Compound("CsF", "Cesium Fluoride");
+    Compound ionic5 = new Compound("LiCl", "Lithium Chloride");
+    Compound ionic6 = new Compound("KI", "Potassium Iodide");
+    Compound ionic7 = new Compound("NaF", "Sodium Fluoride");
+    Compound ionic8 = new Compound("NaHCO3", "Sodium Bicarbonate");
+    Compound ionic9 = new Compound("Na2CO3", "Sodium Carbonate");
+    Compound ionic10 = new Compound("NaOCl", "Sodium Hypochlorite");
 
     public ArrayList<String> mixFormula(String formula) {
         Random random = new Random();
@@ -30,28 +37,29 @@ public class CompoundIonic {
                 formulaLength -= 1;
                 formulaMixed.append(randomCharacter);
             }
-
-
             formulasToDisplay.add(formulaMixed.toString());
             formulaToMix = formula;
             formulaLength = formula.length();
         }
-//        Random random = new Random();
-//        ArrayList<String> formulasToDisplay = new ArrayList<>();
-//        String formulaToMix = formula;
-//        formulasToDisplay.add(formula);
-//
-//        for (int i = 0; i < 3; i++) {
-//            StringBuilder mixedFormula = new StringBuilder();
-//            int formulaLength = formula.length();
-//            for (int j = 0; j < formula.length(); j++) {
-//                char randomElement = formula.charAt(random.nextInt(formulaLength));
-//                formulaToMix = formulaToMix.substring(0, )
-//                mixedFormula.append(randomElement);
-//                formulaLength -= 1;
-//            }
-//            formulasToDisplay.add(mixedFormula.toString());
-//        }
+        Collections.shuffle(formulasToDisplay);
         return formulasToDisplay;
+    }
+
+    public ArrayList<Compound> getIonicCompounds() {
+        ArrayList<Compound> ionicCompounds = new ArrayList<>();
+
+        ionicCompounds.add(ionic1);
+        ionicCompounds.add(ionic2);
+        ionicCompounds.add(ionic3);
+        ionicCompounds.add(ionic4);
+        ionicCompounds.add(ionic5);
+        ionicCompounds.add(ionic6);
+        ionicCompounds.add(ionic7);
+        ionicCompounds.add(ionic8);
+        ionicCompounds.add(ionic9);
+        ionicCompounds.add(ionic10);
+
+        Collections.shuffle(ionicCompounds);
+        return ionicCompounds;
     }
 }
