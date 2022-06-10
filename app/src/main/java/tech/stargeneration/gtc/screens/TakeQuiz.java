2e81d0dceccdbcc2893602994d6bce0a79c75185
ritznotcracker <ritznotcracker@gmail.com>
2022-06-10 19:24:32 +0800

    add: take quiz activity

---
+++ app/src/main/java/tech/stargeneration/gtc/screens/TakeQuiz.java
@@ -0,0 +1,14 @@
+package tech.stargeneration.gtc.screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import tech.stargeneration.gtc.R;
+
+public class TakeQuiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_take_quiz);
+    }
+}
