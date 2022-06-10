0003229b13528a9e6d0c549f09d30c179e75a3e0
ritznotcracker <ritznotcracker@gmail.com>
2022-06-10 16:38:21 +0800

    add: test for buttons

--- app/src/main/java/tech/stargeneration/gtc/screens/ChooseCompunds.java
+++ app/src/main/java/tech/stargeneration/gtc/screens/ChooseCompunds.java
@@ -1,5 +1,8 @@
 package tech.stargeneration.gtc.screens;
 
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import tech.stargeneration.gtc.R;
@@ -10,5 +13,12 @@ public class ChooseCompunds extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_compunds);
+
+        RelativeLayout btnIonic = findViewById(R.id.btnIonic);
+        TextView textView = findViewById(R.id.textView);
+
+        btnIonic.setOnClickListener(event -> {
+            textView.setText("Ionic");
+        });
     }
 }
