0307ae78b86a438c7aaf60d2e300ef823a5c88b1
ritznotcracker <ritznotcracker@gmail.com>
2022-06-10 23:07:04 +0800

    finish: scoreboard

--- app/src/main/java/tech/stargeneration/gtc/screens/ShowScore.java
+++ app/src/main/java/tech/stargeneration/gtc/screens/ShowScore.java
@@ -1,5 +1,6 @@
 package tech.stargeneration.gtc.screens;
 
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import tech.stargeneration.gtc.R;
@@ -10,5 +11,23 @@ public class ShowScore extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_score);
+
+        Bundle extras = getIntent().getExtras();
+        TextView scoreView = findViewById(R.id.scoreView);
+        TextView message = findViewById(R.id.message);
+        String messageToShow;
+
+        int score = extras.getInt("QUIZ_SCORE");
+
+        if (score <= 5) {
+            messageToShow = "Again!";
+        } else if (score <=8) {
+            messageToShow = "Nice!";
+        } else {
+            messageToShow = "Good job!";
+        }
+
+        message.setText(messageToShow);
+        scoreView.setText(score + "/10");
     }
 }
