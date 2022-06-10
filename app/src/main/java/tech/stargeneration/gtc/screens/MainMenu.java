97d14996077a2aeaa452b3f35a9f92dbea852a88
ritznotcracker <ritznotcracker@gmail.com>
2022-06-10 14:09:07 +0800

    function: move to next activity

--- app/src/main/java/tech/stargeneration/gtc/screens/MainMenu.java
+++ app/src/main/java/tech/stargeneration/gtc/screens/MainMenu.java
@@ -1,5 +1,7 @@
 package tech.stargeneration.gtc.screens;
 
+import android.content.Intent;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import tech.stargeneration.gtc.R;
@@ -10,5 +12,15 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        Button startBtn = findViewById(R.id.btnStart);
+        Button learnBtn = findViewById(R.id.btnLearn);
+
+        Intent chooseCompounds = new Intent(this, ChooseCompunds.class);
+        Intent learnSection = new Intent(this, ChooseCompunds.class);
+
+        startBtn.setOnClickListener(action -> {
+            startActivity(chooseCompounds);
+        });
     }
 }
