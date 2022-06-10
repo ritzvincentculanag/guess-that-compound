8c85319899ee2fd376e60908cd889e926ecb40e5
ritznotcracker <ritznotcracker@gmail.com>
2022-06-10 13:56:44 +0800

    initial commit

---
+++ app/src/main/java/tech/stargeneration/gtc/screens/MainMenu.java
@@ -0,0 +1,14 @@
+package tech.stargeneration.gtc.screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import tech.stargeneration.gtc.R;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
